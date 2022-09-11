aa844fb7eba992e06c6a53b094fb37a1d66f3503
Saurabh Sharma <saurabh.sharma@sbr-technologies.com>
2022-09-11 18:30:03 +0530

    Added Test cases

--- SwiftyLib/SwiftyLib.swift
+++ SwiftyLib/SwiftyLib.swift
@@ -8,10 +8,14 @@
 import Foundation
 
 
-public class finalSwiftyLib {
-    public init() {
-        TestFile()
+public class FinalSwiftyLib {
+  
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
     }
     
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
     
 }
