8907221a2062bd3599b437f1e5b9c862003baf37
Saurabh Sharma <saurabh.sharma@sbr-technologies.com>
2022-09-11 17:39:41 +0530

    First Commit

---
+++ Shared/SwiftyLibExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLibExampleApp.swift
+//  Shared
+//
+//  Created by Saurabh Sharma on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyLibExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
