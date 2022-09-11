f40f98e68d5e2df026ff79c618e24f1ae5a3fae1
Saurabh Sharma <saurabh.sharma@sbr-technologies.com>
2022-09-11 18:11:53 +0530

    Some new file added

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Saurabh Sharma on 11/09/22.
+//
+
+import Foundation
+
+
+public class finalSwiftyLib {
+    public init() {
+        TestFile()
+    }
+    
+    
+}
---
+++ SwiftyLib/TestFile.swift
@@ -0,0 +1,14 @@
+//
+//  TestFile.swift
+//  SwiftyLib
+//
+//  Created by Saurabh Sharma on 11/09/22.
+//
+
+import Foundation
+
+public final class TestFile {
+    public init() {
+        debugPrint("This is a Test File and POD is successfully installed")
+    }
+}
